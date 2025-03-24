11bfa54ab07cdde0671848edf13e92920974daa2
gomezc1 <75422709+gomezc1@users.noreply.github.com>
2025-03-24 03:16:03 -0700

    code

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function formatDate(date) {
+
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
